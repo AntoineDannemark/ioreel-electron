80b421a6a38befa63e18efda64c4f4d2358ab7ef
Antoine <antoine.dannemark@protonmail.com>
2021-02-01 14:36:37 +0100

    fix args passing @preloader/update

--- preloader.js
+++ preloader.js
@@ -8,7 +8,7 @@ contextBridge.exposeInMainWorld(
       initDB: platform => ipcRenderer.invoke('init-db', platform),
       createTenant: tenant => ipcRenderer.invoke('create-tenant', tenant),
       fetchTenants: () => ipcRenderer.invoke('fetch-tenants'),
-      updateTenant: (id, data) => ipcRenderer.invoke('update-tenant', (id, data)),
+      updateTenant: (id, data) => ipcRenderer.invoke('update-tenant', {id, ...data}),
       removeTenant: id => ipcRenderer.invoke('remove-tenant', id),
     }
   )
